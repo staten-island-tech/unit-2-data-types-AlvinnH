109de63a9319839db1913537abb87af305fa5803
AlvinnH <alvinh27@nycstudents.net>
2024-02-11 23:46:11 -0500

    Update GCF.py

--- GCF.py
+++ GCF.py
@@ -1,12 +1,8 @@
-x = int(input('First Number'))
-y = int(input('Second Number'))
+x = int(input('First Number: '))
+y = int(input('Second Number: '))
 def gcf(x,y):
-    if x > y:
-            smaller = y
-    else:
-          smaller = x
     for i in range(1,x+1):
           if (x % i == 0) and (y % i == 0):
-                hcf = i
-    return(hcf)
+                gcf = i
+    return(gcf)
 print(gcf(x,y))
