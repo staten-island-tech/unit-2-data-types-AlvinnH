4d5c848088e1716fec3cb359b1d2d163dcda94d4
AlvinnH <alvinh27@nycstudents.net>
2024-02-11 22:13:07 -0500

    Update TipCalculator.py

--- TipCalculator.py
+++ TipCalculator.py
@@ -1,4 +1,4 @@
 bill = input("What is the bill?")
-tips = input("How much do you want to tip?")
+tips = input("How much do you want to tip? (0-1)")
 total = int(float(bill) * float(tips)) + float(bill)
 print(total)
