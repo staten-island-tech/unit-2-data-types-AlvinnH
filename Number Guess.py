4fcb684db54dae6e109a47d1467ccc029fa70727
AlvinnH <alvinh27@nycstudents.net>
2024-02-12 14:57:03 -0500

    Update Number Guess.py

--- Number Guess.py
+++ Number Guess.py
@@ -1,19 +1,19 @@
 import random
 
-number_list = ["1","2","3","4","5","6","7","8","9","10"]
+number_list = ["1", "2","3","4","5","6","7","8","9","10"]
 random_number = random.choice(number_list)
 wrong_list = []
 correct = False
 while correct == False:
-    guess = input(random_number)
+    guess = input("Guess a number from 1-10 ")
     if guess == random_number:
-        print("You have guessed the number!")
+        print("You have guessed correctly")
         print(wrong_list)
         correct = True
     elif guess != random_number:
         wrong_list.append(guess)
-print(wrong_list)
-if guess > random_number:
-    print("Your guess is greater than the random number")
-elif guess < random_number:
-    print("Your guess is lower than the random number")
+        print(wrong_list)
+        if guess > random_number:
+            print("The number is smaller than the one you guessed")
+        elif guess < random_number:
+            print("The number is bigger than the one you guessed")
